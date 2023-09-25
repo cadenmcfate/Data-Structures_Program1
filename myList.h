ff5e9ae5c7adb3872bc7db8f2c552462f48aff01
cadenmcfate <cmcfate@CadensSurface.localdomain>
2023-09-24 19:14:03 -0600

    updated files

--- myList.h
+++ myList.h
@@ -1,9 +1,8 @@
 //myList.h
-//defines the linked list template class
+//defines the linked list template class for program 1
 //Caden McFate
 //Programming assignment 1
 //COSC 2030
-
 #ifndef MYLIST_
 #define MYLIST_
 #include "node.h"
@@ -16,14 +15,14 @@ private:
 	node<T>* head;
 	int size;
 public:
-	myList() { head = nullptr; size = 0; }
-	~myList();
-	void insert(T item);
+	myList() { head = nullptr; size = 0; } //constructor
+	~myList(); //destructor
+	void insert(T item); //insert front
 	T front();
-	int find(T item);
-	void sort(int max);
-	void display();
-	void remove();
+	int find(T item); //returns the amount of string.compare(string) operations it took to find 'item'. Positive if found, negative if not found.
+	void sort(int max); //sorts the list by increasing length.
+	void display(); //prints the list
+	void remove(); //remove front
 	bool isEmpty() { return size == 0;  }
 	int getSize() { return size; }
 };
@@ -72,16 +71,15 @@ int myList<T>::find(T item) {
 	node<T>* tmp = head;
 	int compares = 0;
 	while (tmp != nullptr) { 
-		if ((tmp->data.length() != item.length()) || (int(tmp->data[0]) != int(item[0]))) { tmp = tmp->next; continue; } //move on if the words aren't the same length or don't start with the same letter
+		if (tmp->data.length() != item.length()) { tmp = tmp->next; continue; } //move on if the words aren't the same length
+		if (int(tmp->data[0]) != int(item[0])) { tmp = tmp->next; continue; } //move on if the words don't start with the same letter
 		compares++;
 		if (item.compare(tmp->data) == 0) { 
-			return compares; //word is found
+			return compares; //word is found, compares is returned as a positive value.
 		} 
-		else {
 		tmp = tmp->next;
-		}
 	}
-	return compares * -1; //compares is returned as a negative to distinguish a word-not-found from a found word.
+	return compares * -1; //compares is returned as a negative value to distinguigh regular compares from the 'word-not-found' compares.
 }
 
 
@@ -99,7 +97,7 @@ void myList<T>::sort(int max) {
 	node<T>* tmp = head;
 	myList<T> sorted;
 	int checkLength = max;
-	while (checkLength >= 1) {	//sorting the dictionary in increasing word length
+	while (checkLength >= 1) {	//sorting the dictionary in increasing word length to the sorted linked list.
 		tmp = head;
 		while (tmp != nullptr) {
 			if (tmp->data.length() == checkLength) {
@@ -111,7 +109,7 @@ void myList<T>::sort(int max) {
 	}
 	node<T>* sorttmp = sorted.head;		//copying the sorted list back into the original dictionary
 	tmp = head;
-	while (sorttmp != nullptr) {
+	while (sorttmp != nullptr && tmp != nullptr) {
 		tmp->data = sorttmp->data;
 		sorttmp = sorttmp->next;
 		tmp = tmp->next;
